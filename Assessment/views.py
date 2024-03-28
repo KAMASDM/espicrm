b099c45147036c4f18dadc4875e2550e4de0a595
Ramesh244 <ramesh.talluri4@gmail.com>
2024-03-28 13:55:48 +0530

    restapis

--- Assessment/views.py
+++ Assessment/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import assessment
+from .serializers import AssessmentSerializer
+
+class AssessmentViewSet(viewsets.ModelViewSet):
+    queryset = assessment.objects.all()
+    serializer_class = AssessmentSerializer
